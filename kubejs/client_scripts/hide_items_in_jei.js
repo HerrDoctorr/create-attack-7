8d18f8169eda943b4178916d05160a0a2e4b0bcd
Hajsori <108522996+Hajsori@users.noreply.github.com>
2026-04-19 15:19:54 +0200

    Delete kubejs/client_scripts/hide_items_in_jei.js
    kubejs/server_scripts/hide_items.js is now hiding items

--- kubejs/client_scripts/hide_items_in_jei.js
+++
@@ -1,9 +0,0 @@
-JEIEvents.hideItems((event) => {
-    event.hide("create_attack_7:incomplete_netherite_upgrade_smithing_template")
-
-    event.hide("create_attack_7:incomplete_engine")
-    event.hide("create_attack_7:incomplete_boiler")
-    event.hide("create_attack_7:incomplete_eco_engine")
-    event.hide("create_attack_7:incomplete_sturdy_pipes")
-    event.hide("create_attack_7:incomplete_gyroscope")
-})
